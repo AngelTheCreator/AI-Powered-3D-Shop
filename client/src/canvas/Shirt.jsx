04e8b6f096f8b65c29d3fb080b7a0b3eeb4c6747
Ángel Guevara <angel_guevara94@hotmail.com>
2024-05-26 23:13:53 -0700

    feat: Add Shirt component to canvas

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+import { useSnapshot } from 'valtio'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
+import state from '../store'
+
+const Shirt = () => {
+  return (
+    <div>Shirt</div>
+  )
+}
+
+export default Shirt
