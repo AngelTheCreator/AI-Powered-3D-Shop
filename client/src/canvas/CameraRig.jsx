a21b0c49952975a843c5d5987df696f5a726d72c
Ángel Guevara <angel_guevara94@hotmail.com>
2024-05-26 23:57:57 -0700

    refactor: Optimize CameraRig component in canvas

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
 
-const CameraRig = () => {
+import state from '../store'
+
+const CameraRig = ({ children }) => {
+    const snap = useSnapshot(state);
+    const group = useRef();
+
+    useFrame((state, delta) => {
+        
+        //set the model rotation smoothly
+        easing.dampE(
+            group.current.rotation,
+            [state.pointer.y / 10, -state.pointer.x / 5, 0],
+            0.25,
+            delta
+        )
+    });
+
+    
+
+    
   return (
     <div>CameraRig</div>
   )
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -10,9 +10,11 @@ import CameraRig from './CameraRig'
         <Canvas>
             <ambientLight intensity={0.5} />
             <Environment preset='city' />
+            <CameraRig>
             <Center>
                 <Shirt />
             </Center>
+            </CameraRig>
         </Canvas>
     )
 }
