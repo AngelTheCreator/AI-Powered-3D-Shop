465b9d5e5b526b2591a14ad4667b30213e4c741d
Ángel Guevara <angel_guevara94@hotmail.com>
2024-05-27 00:08:07 -0700

    shirt is now rendering and the camera rig works well, position changes based on page

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -10,6 +10,22 @@ const CameraRig = ({ children }) => {
     const group = useRef();
 
     useFrame((state, delta) => {
+        const isBreakpoint = window.innerWidth <= 1260;
+        const isMobile = window.innerWidth <= 600;
+
+        //set the initial position of the model
+        let targetPosition = [-0.4, 0, 2];
+        if(snap.intro){
+            if(isBreakpoint) targetPosition = [0, 0, 2];
+            if(isMobile) targetPosition = [0, 0.2, 2.5];
+        } else {
+            if(isMobile) targetPosition = [0, 0, 2.5];
+            else targetPosition = [0, 0, 2];
+        }
+
+        //set model camera positon
+
+        easing.damp3(state.camera.position, targetPosition, 0.25, delta);
         
         //set the model rotation smoothly
         easing.dampE(
@@ -24,7 +40,7 @@ const CameraRig = ({ children }) => {
 
     
   return (
-    <div>CameraRig</div>
+    <group ref={group}>{children}</group>
   )
 }
 
