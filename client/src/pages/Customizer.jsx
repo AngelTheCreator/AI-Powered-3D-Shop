9a137eb3c26b873b4d9447b38ffb43b84ddcd4ee
Ángel Guevara <angel_guevara94@hotmail.com>
2024-05-26 21:57:50 -0700

    feat: Add Go Back button to Customizer page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -16,6 +16,7 @@ const Customizer = () => {
   return (
     <AnimatePresence>
       {!snap.intro && (
+        <>
         <motion.div
         key='custom'
         className='absolute top-0 left-0 z-10'
@@ -33,6 +34,18 @@ const Customizer = () => {
             </div>
           </div>
         </motion.div>
+
+        <motion.div
+        className='absolute z-10 top-5 right-5'
+        {...fadeAnimation}>
+          <CustomButton
+            type='filled'
+            title='Go Back'
+            handleClick={() => state.intro = true}
+            customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+          />
+        </motion.div>
+        </>
       )}
     </AnimatePresence>
   )
